d84b393c546dc9c0f4584d81ec9ace0a462daad8
rol3x <rol3x@macbook-de-ronan-alexandre.local>
2011-09-06 17:27:15 +0200

    [SHADER] Better scene with some correction on phong shader

--- opengl/shader/samples/vertex_shaders/phong_point.vert
+++ opengl/shader/samples/vertex_shaders/phong_point.vert
@@ -3,7 +3,7 @@ varying vec3 normal, vertex_vector;
 void main(void)
 {
   normal = gl_NormalMatrix * gl_Normal;
-  vertex_vector = normalize(vec3(gl_ModelViewMatrix * gl_Vertex));
+  vertex_vector = vec3(gl_ModelViewMatrix * gl_Vertex);
 
   gl_Position = ftransform();
 }
--- opengl/shader/simple_shader_loader/main.c
+++ opengl/shader/simple_shader_loader/main.c
@@ -14,6 +14,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 #include <GL/glew.h>
 
@@ -25,8 +26,11 @@
 
 #include "shader_samples.h"
 
-#define SPEED_LIGHT 0.1
-double angle = 0;
+#define SPEED_LIGHT 0.005
+
+enum AXES {X = 0, Y, Z, H};
+
+double angle_rad = 0;
 
 void init()
 {
@@ -51,18 +55,16 @@ void init_light(void)
   float light_ambient[4] = {.2, .2, .2, 1.};
   float light_diffuse[4] = {1., 1., 1., 1.};
   float light_specular[4] = {1., 1., 1., 1.};
-  float light_position[4] = {0., 0., 2., 1.};
 
   glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
   glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
   glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-  glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 
   // Material parameters:
   float material_ambient[4] = {.2, .2, .2, 1.};
   float material_diffuse[4] = {1., 1., 1., 1.};
   float material_specular[4] = {1., 1., 1., 1.};
-  float material_shininess = 100.;
+  float material_shininess = 50.;
 
   glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, material_ambient);
   glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_diffuse);
@@ -70,26 +72,49 @@ void init_light(void)
   glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, material_shininess);
 }
 
+void move_light(void)
+{
+  float light_position[4] = {2., 0., 0., 1.};
+
+  float light_norme = sqrt(
+      (light_position[X] * light_position[X]) +
+      (light_position[Y] * light_position[Y]) +
+      (light_position[Z] * light_position[Z]));
+
+  float x = cos(angle_rad) * light_norme;
+  float z = sin(angle_rad) * light_norme;
+  
+  light_position[X] = x;
+  light_position[Z] = z;
+
+  glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+  glColor3f(1., 1., 1.);
+  glTranslatef(light_position[X] + .5,
+      light_position[Y],
+      light_position[Z] + .5);
+  glutSolidSphere(.1, 50, 50);
+
+  angle_rad += SPEED_LIGHT;
+}
+
 void display()
 { 
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glMatrixMode(GL_MODELVIEW);
   glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+  init_light();
 
   glColor3f(1., 0., 0.);
 
+  init_light();
+
   // Put Cam and Sphere
   glLoadIdentity();
-  gluLookAt(0., 0., 5., 0., 0., 0., 0., 1., 0.);
+  gluLookAt(0., 2., 5., 0., 0., 0., 0., 1., 0.);
+
   glutSolidTeapot(1);
-
-  // Rotate and put light
-  glRotatef(angle, 0., 1., 0.);
-  init_light();
-  // glTranslatef(0., 0., 2.5);
-  // glutSolidSphere(.1, 50, 50);
-
-  angle += SPEED_LIGHT;
+  move_light();
 
   glutSwapBuffers(); 
   glutPostRedisplay();
@@ -148,8 +173,8 @@ int main(int argc, char **argv)
   // gouraud_shader();
   // cel_shader();
   // uniform_shader();
-  // xray_shader();
-  // phong_shader();
+  xray_shader();
+  phong_shader();
 
   glutMainLoop();
   return 0;
