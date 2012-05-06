d187a9e6dd8b9b5674f46eb67d7358bb3ee1ee88
Ronan Cherrueau <ronancherrueau@gmail.com>
2012-05-06 05:13:19 +0200

    Test qith Nodejs

---
+++ nodejs/simpleHttpServer/simpleHttpServer.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+}).listen(1337);
+
+console.log('Server running at http://127.0.0.1:1337/');
+
