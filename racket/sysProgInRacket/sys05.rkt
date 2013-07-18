86a7ae215e8fa3e887f5da35897a7c0736f16c6d
Ronan-Alexandre Cherrueau <RonanCherrueau@gmail.com>
2013-07-18 23:24:02 +0200

    Use let instead of define

--- racket/sysProgInRacket/sys05.rkt
+++ racket/sysProgInRacket/sys05.rkt
@@ -7,19 +7,19 @@
 #lang racket
 
 ; serve: port-no -> stop
-; Start a new TCP server listening on port port-no. Serve 
+; Start a new TCP server listening on port port-no. Serve
 ; returns a function that can be used to shut down the server.
 (define (serve port-no)
-  
+
   ; Create a "listening" server on the local machin with
   ; maximum 5 simultaneous connection.
   (define listener (tcp-listen port-no 5 #t))
-  
+
   ; Loop to accept connections from listeners.
   (define (loop)
     (accept-and-handle listener)
     (loop))
-  
+
   ; Listener loop in its own thread, so
   ; (serve port-no) in repl will return immediately.
   (define t (thread loop))
@@ -34,13 +34,14 @@
 ; Accept a connection and process input (request)
 ; and output (respone).
 (define (accept-and-handle listener)
-  (define-values (in out) (tcp-accept listener))
-  (handle in out)
-  (close-input-port in)
-  (close-output-port out))
+  (let-values
+      ([(in out) (tcp-accept listener)])
+    (handle in out)
+    (close-input-port in)
+    (close-output-port out)))
 
 ; handle: in out -> doesn't return
-; Read and discard the request header and then write 
+; Read and discard the request header and then write
 ; "Hello World!" web page as a result.
 (define (handle in out)
   ; Discard the request header (up to blank line
