a485ee7c6bc20d17af01b02f6bf352eda0878bef
Ronan-Alexandre Cherrueau <RonanCherrueau@gmail.com>
2013-07-21 00:55:14 +0200

    Racket SysProg: Use custodian to manage resources

--- racket/sysProgInRacket/sys07.rkt
+++ racket/sysProgInRacket/sys07.rkt
@@ -10,44 +10,47 @@
 ; Start a new TCP server listening on port port-no. Serve 
 ; returns a function that can be used to shut down the server.
 (define (serve port-no)
+  (define main-cust (make-custodian))
+  (parameterize ([current-custodian main-cust])
+    ; Create a "listening" server on the local machin with
+    ; maximum 5 simultaneous connection.
+    (define listener (tcp-listen port-no 5 #t))
   
-  ; Create a "listening" server on the local machin with
-  ; maximum 5 simultaneous connection.
-  (define listener (tcp-listen port-no 5 #t))
+    ; Loop to accept connections from listeners.
+    (define (loop)
+      (accept-and-handle listener)
+      (loop))
   
-  ; Loop to accept connections from listeners.
-  (define (loop)
-    (accept-and-handle listener)
-    (loop))
-  
-  ; Listener loop in its own thread, so
-  ; (serve port-no) in repl will return immediately.
-  (define t (thread loop))
+    ; Listener loop in its own thread, so
+    ; (serve port-no) in repl will return immediately.
+    (thread loop))
 
-  ; Function for shut down the server thread and
-  ; tcp listener
+  ; Function for shut down the server and close 
+  ; resources in the custodian.
   (lambda ()
-    (kill-thread t)
-    (tcp-close listener)))
+    (custodian-shutdown-all main-cust)))
 
 ; accept-and-handle: listener -> in out
 ; Accept a connection and process input (request)
 ; and output (respone).
 (define (accept-and-handle listener)
-  (define-values (in out) (tcp-accept listener))
-  (define t (thread
-             (lambda ()
-               ;; (sleep (random 10))
-               (handle in out)
-               (close-input-port in)
-               (close-output-port out))))
+  ; Container for thread and in out streams 
+  (define cust (make-custodian))
+  (parameterize ([current-custodian cust])
+    (define-values (in out) (tcp-accept listener))
+    (thread
+     (lambda ()
+       ;; (sleep (random 10))
+       (handle in out)
+       (close-input-port in)
+       (close-output-port out))))
   
-  ; Wait for 10 second then kill t.
+  ; Wait for 10 second then close all elements in the custodian.
   ; This is a poor implementation of TTL.
   (thread
    (lambda()
      (sleep 10)
-     (kill-thread t)))
+     (custodian-shutdown-all cust))))
 
 ; handle: in out -> doesn't return
 ; Read and discard the request header and then write 
