2751243978afd8156a5c8deea73f19c03d9967a6
Ronan-Alexandre Cherrueau <RonanCherrueau@gmail.com>
2013-08-08 15:00:02 +0200

    Racket, tree: add documentation

--- racket/fileSystem/fs03.rkt
+++ racket/fileSystem/fs03.rkt
@@ -5,15 +5,26 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 #lang racket
 
+;; tree: file-path -> void
+;; Recursive directory listing.
 (define (tree file-path)
+
+  ;; get-file-type: a-file-path -> file-type
+  ;; Consumes a file path and return the type of file.
+  ;; 'file if file-path targets a file
+  ;; 'dire if file-paht targets a directory
+  ;; 'link if file-path targets a link
   (define (get-file-type a-file-path)
     (cond
      [(file-exists? a-file-path) 'file]
      [(directory-exists? a-file-path) 'dir]
-     [(link-exists? a-file-path) 'link]
-     [else (write a-file-path)]))
+     [(link-exists? a-file-path) 'link]))
 
-  (define (indent level a-string [final? #f])
+  ;; indent: level file-path-name level [final?] -> indented-file-path-name
+  ;; Indent file-path-name with the correct level of indentation. If
+  ;; file is the last of it's sub-tree (final? to #t), the indentation
+  ;; system use specific character.
+  (define (indent file-path-name level [final? #f])
     (define (make-right-part level [right-part ""])
       (cond
        [(<= level 1) right-part]
@@ -21,25 +32,27 @@
         (make-right-part (- level 1) (string-append "│   " right-part))]))
 
     (cond
-     [(<= level 0) a-string]
+     [(<= level 0) file-path-name]
      [(= level 1)
-      (string-append (if final? "└── " "├── ") a-string)]
+      (string-append (if final? "└── " "├── ") file-path-name)]
      [(> level 1)
       (string-append (make-right-part level)
                      (if final? "└── " "├── ")
-                     a-string)]))
+                     file-path-name)]))
 
+  ;; tree-rec: a-file-path file-ype level file-id -> void
+  ;; Recursive directory listing.
   (define (tree-rec a-file-path file-type level file-id)
     (cond
      [(eq? 'file file-type)
       (define-values (base name must-be-dir?) (split-path a-file-path))
-      (displayln (indent level
-                         (path->string name)
+      (displayln (indent (path->string name)
+                         level
                          (if (<= file-id 0) #t #f)))]
      [(eq? 'dir file-type)
       (define-values (base name must-be-dir?) (split-path a-file-path))
-      (displayln (indent level
-                         (path->string name)
+      (displayln (indent (path->string name)
+                         level
                          (if (<= file-id 0) #t #f)))
 
       (define sub-file-paths (directory-list a-file-path))
