494cec8a81e90e3cc139cd430f077d2705fc7854
Ronan-Alexandre Cherrueau <RonanCherrueau@gmail.com>
2013-08-08 14:12:16 +0200

    Update tree for pure Functional Programming

--- racket/fileSystem/fs03.rkt
+++ racket/fileSystem/fs03.rkt
@@ -35,25 +35,27 @@
       (define-values (base name must-be-dir?) (split-path a-file-path))
       (displayln (indent level
                          (path->string name)
-                         (if (<= file-id 1) #t #f)))]
+                         (if (<= file-id 0) #t #f)))]
      [(eq? 'dir file-type)
       (define-values (base name must-be-dir?) (split-path a-file-path))
       (displayln (indent level
                          (path->string name)
-                         (if (<= file-id 1) #t #f)))
+                         (if (<= file-id 0) #t #f)))
 
       (define sub-file-paths (directory-list a-file-path))
-      (define sub-file-paths-id (length sub-file-paths))
+      (define sub-file-paths-ids (reverse (range (length sub-file-paths))))
+      (define (directory-listing sub-file-path file-ids)
+        (define new-file-path (build-path a-file-path sub-file-path))
 
-      (map (lambda (sub-file-path)
-             (define new-file-path (build-path a-file-path
-                                               sub-file-path))
-             (tree-rec new-file-path
-                       (get-file-type new-file-path)
-                       (+ level 1)
-                       sub-file-paths-id)
-             (set! sub-file-paths-id (- sub-file-paths-id 1)))
-           sub-file-paths)]))
+        (tree-rec new-file-path
+                  (get-file-type new-file-path)
+                  (add1 level)
+                  (first file-ids))
+
+        (rest file-ids))
+
+      (foldl directory-listing sub-file-paths-ids sub-file-paths)]))
+
 
   (define file-type (get-file-type file-path))
   (cond
@@ -63,15 +65,15 @@
     (displayln file-path)
 
     (define sub-file-paths (directory-list file-path))
-    (define sub-file-paths-id (length sub-file-paths))
+    (define sub-file-paths-ids (reverse (range (length sub-file-paths))))
+    (define (directory-listing a-file-path file-ids)
+      (define new-file-path (build-path file-path a-file-path))
 
-    (map (lambda (sub-file-path)
-           (define new-file-path (build-path file-path
-                                             sub-file-path))
-           (tree-rec new-file-path
-                     (get-file-type new-file-path)
-                     1
-                     sub-file-paths-id)
-           (set! sub-file-paths-id (- sub-file-paths-id 1)))
-         sub-file-paths)
-    (display "")]))
+      (tree-rec new-file-path
+                (get-file-type new-file-path)
+                1
+                (first file-ids))
+
+      (rest file-ids))
+
+    (void (foldl directory-listing sub-file-paths-ids sub-file-paths))]))
