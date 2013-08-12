b1b2eb4133076db332acce82f26c6ad5f1189f52
Ronan-Alexandre Cherrueau <RonanCherrueau@gmail.com>
2013-08-12 16:50:38 +0200

    Correct indentation system

--- racket/fileSystem/fs03.rkt
+++ racket/fileSystem/fs03.rkt
@@ -9,10 +9,19 @@
 ;; Recursive directory listing.
 (define (tree file-path)
 
+  (define (levels-add-not-final levels)
+    (append levels (list 'not-final)))
+
+  (define (levels-add-final levels)
+    (append levels (list 'final)))
+
+  (define (final-level? level)
+    (eq? 'final level))
+
   ;; get-file-type: a-file-path -> file-type
   ;; Consumes a file path and return the type of file.
   ;; 'file if file-path targets a file
-  ;; 'dire if file-paht targets a directory
+  ;; 'dir if file-paht targets a directory
   ;; 'link if file-path targets a link
   (define (get-file-type a-file-path)
     (cond
@@ -26,35 +35,34 @@
   ;; file is the last of it's sub-tree (final? to #t), the indentation
   ;; system use specific character. level is the level of indentation.
   ;; final-level is the level of final indentation.
-  (define (indent file-path-name level final-level [final? #f])
-    (define (make-right-part type level [right-part ""])
+  (define (indent file-path-name levels [final? #f])
+    (define (make-right-part levels [right-part ""])
       (cond
-       [(<= level 1) right-part]
-       [(> level 1)
-        (make-right-part type (- level 1) (string-append type right-part))]))
+       [(empty? levels) right-part]
+       [(final-level? (first levels))
+        (make-right-part (rest levels) (string-append right-part "    " ))]
+       [else
+        (make-right-part (rest levels) (string-append right-part "│   " ))]))
 
     (cond
-     [(<= level 0) file-path-name]
-     [(= level 1)
-      (string-append (if final? "└── " "├── ") file-path-name)]
-     [(> level 1)
-      (string-append (make-right-part "│   " (- level final-level))
-                     (make-right-part "    " final-level)
+     [(empty? levels) file-path-name]
+     [else
+      (string-append (make-right-part levels)
                      (if final? "└── " "├── ")
                      file-path-name)]))
 
   ;; tree-rec: a-file-path file-ype level file-id -> void
   ;; Recursive directory listing.
-  (define (tree-rec a-file-path file-type level final-level file-id)
+  (define (tree-rec a-file-path file-type levels file-id)
     (define final? (<= file-id 0))
 
     (cond
      [(eq? 'file file-type)
       (define-values (base name must-be-dir?) (split-path a-file-path))
-      (displayln (indent (path->string name) level final-level final?))]
+      (displayln (indent (path->string name) levels final?))]
      [(eq? 'dir file-type)
       (define-values (base name must-be-dir?) (split-path a-file-path))
-      (displayln (indent (path->string name) level final-level final?))
+      (displayln (indent (path->string name) levels final?))
 
       (define sub-file-paths (directory-list a-file-path))
       (define sub-file-paths-ids (reverse (range (length sub-file-paths))))
@@ -63,13 +71,14 @@
 
         (tree-rec new-file-path
                   (get-file-type new-file-path)
-                  (add1 level)
-                  (if final? (add1 final-level) final-level)
+                  (if final?
+                      (levels-add-final levels)
+                      (levels-add-not-final levels))
                   (first file-ids))
 
         (rest file-ids))
 
-      (foldl directory-listing sub-file-paths-ids sub-file-paths)]))
+      (void (foldl directory-listing sub-file-paths-ids sub-file-paths))]))
 
 
   (define file-type (get-file-type file-path))
@@ -86,8 +95,7 @@
 
       (tree-rec new-file-path
                 (get-file-type new-file-path)
-                1
-                0
+                empty
                 (first file-ids))
 
       (rest file-ids))
