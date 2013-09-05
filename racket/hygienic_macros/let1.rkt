7e24a278979bb787fd71ddf4d0b883692af42880
Ronan-Alexandre Cherrueau <RonanCherrueau@gmail.com>
2013-09-05 19:37:47 +0200

    [Racket][Syntax] let

---
+++ racket/hygienic_macros/let1.rkt
@@ -0,0 +1,21 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Writing Hygienic Macros in Scheme with Syntax-case
+;; R. Kent Dybvig
+;; Technical Report #356
+;;
+;; page 9
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+#lang racket
+
+(require "print-test.rkt")
+
+; Ellipse of pattern `[i v] ...` can be split up in the output as
+; `i ...` and `v ...`
+(define-syntax (let1 x)
+  (syntax-case x ()
+    [(_ ([i v] ...) e1 e2 ...)
+     #'((lambda (i ...) e1 e2 ...) v ...)]))
+
+(print-test (let1 ([v1 '4]
+                   [v2 '2])
+                  (printf "~a~a\n" v1 v2)))
