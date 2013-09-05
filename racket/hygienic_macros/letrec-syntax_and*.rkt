708713214424cee1dc31027f3e47a1b2ec6b75f0
Ronan-Alexandre Cherrueau <RonanCherrueau@gmail.com>
2013-09-05 15:59:44 +0200

    [Racket][Syntax] and*

---
+++ racket/hygienic_macros/letrec-syntax_and*.rkt
@@ -0,0 +1,29 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Writing Hygienic Macros in Scheme with Syntax-case
+;; R. Kent Dybvig
+;; Technical Report #356
+;;
+;; page 7-8
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+#lang racket
+
+(require "print-test.rkt")
+
+; Local macro using let-syntax and letrec-syntax.
+; Here, `letREC-syntax` form is important because macro is
+; recursively-defined. `and*` macro appears in the `syntax` form of
+; the last clause. For this reason, `letREC-syntax` has to be used
+; instead of `let-syntax`
+(letrec-syntax ([and* (lambda (x)
+                        (syntax-case x ()
+                          [(_) #'#t]
+                          [(_ e) #'e]
+                          [(_ e1 e2 e3 ...)
+                           #'(if e1 (and* e2 e3 ...) #f)]))])
+  (let ([x '(a b c d)])
+    (print-test
+     (and* (pair? x) (pair? (cddr x)) (pair? (cdddr x)) (cadddr x))))
+  (print-test
+   (and* #t #f #f #t #t #t #f)
+   (and* #t #t #t)
+   (and*)))
