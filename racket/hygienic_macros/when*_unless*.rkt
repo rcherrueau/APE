0c63a9e4af2b6e2cb264d9d78fb7d7794408ae31
Ronan-Alexandre Cherrueau <RonanCherrueau@gmail.com>
2013-09-05 16:57:55 +0200

    Rename when2 and unless2 to when* and unless*

--- racket/hygienic_macros/when*_unless*.rkt
+++ racket/hygienic_macros/when*_unless*.rkt
@@ -9,33 +9,33 @@
 
 (require "print-test.rkt")
 
-(define-syntax (when2 x)
+(define-syntax (when* x)
   (syntax-case x ()
     [(_ e0 e1 e2 ...)
      #'(if e0 (begin e1 e2 ...) (void))]))
 
-(define-syntax (unless2 x)
+(define-syntax (unless* x)
   (syntax-case x ()
     [(_ e0 e1 e2 ...)
-     #'(when2 (not e0) e1 e2 ...)]))
+     #'(when* (not e0) e1 e2 ...)]))
 
-(print-test (when2 (positive? -5)
+(print-test (when* (positive? -5)
               (display "hi"))
 
-            (when2 (positive? 5)
+            (when* (positive? 5)
               (display "hi")
               (displayln " there"))
 
-            (unless2 (positive? 5)
+            (unless* (positive? 5)
               (display "hi"))
 
-            (unless2 (positive? -5)
+            (unless* (positive? -5)
               (display "hi")
               (displayln " there"))
 
             ; In hygienic macro, the new binding for `begin` and `if`
             ; introduce by the `let` will not alter the semantics of the
-            ; when2.
+            ; when*.
             (let ([begin list]
                   [if 'lala])
-              (when2 #t (write "win") (newline))))
+              (when* #t (write "win") (newline))))
