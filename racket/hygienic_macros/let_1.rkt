1f68fa45d1e0a8f68f11fe238a6cbc7d12ffaaff
Ronan-Alexandre Cherrueau <RonanCherrueau@gmail.com>
2013-09-10 16:24:06 +0200

    Rename let1 macro into let_1 (let version 1)

--- racket/hygienic_macros/let_1.rkt
+++ racket/hygienic_macros/let_1.rkt
@@ -11,11 +11,11 @@
 
 ; Ellipse of pattern `[i v] ...` can be split up in the output as
 ; `i ...` and `v ...`
-(define-syntax (let1 x)
+(define-syntax (let_1 x)
   (syntax-case x ()
     [(_ ([i v] ...) e1 e2 ...)
      #'((lambda (i ...) e1 e2 ...) v ...)]))
 
-(print-test (let1 ([v1 '4]
-                   [v2 '2])
-                  (printf "~a~a\n" v1 v2)))
+(print-test (let_1 ([v1 '4]
+                    [v2 '2])
+                   (printf "~a~a\n" v1 v2)))
