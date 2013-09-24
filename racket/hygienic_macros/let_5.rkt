f752dbd44c273d59129e4f579656888c1e07a90d
Ronan-Alexandre Cherrueau <RonanCherrueau@gmail.com>
2013-09-24 14:48:43 +0200

    "[Racket][macro] let version 5"

--- racket/hygienic_macros/let_5.rkt
+++ racket/hygienic_macros/let_5.rkt
@@ -3,15 +3,15 @@
 ;; R. Kent Dybvig
 ;; Technical Report #356
 ;;
-;; page 10
+;; page 10,11
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 #lang racket
 
 (require "print-test.rkt")
 
-; `all-ids' fender reject input expression that do not have
-; identifiers where they are expected.
-(define-syntax (let_4 x)
+; Move `all-ids?' test into the output expression and call
+; `raise-syntax-error'
+(define-syntax (let_5 x)
   (define (all-ids? lst)
     (define (id? an-id previous-id-res)
       (and (identifier? an-id) previous-id-res))
@@ -19,24 +19,28 @@
 
   (syntax-case x ()
     [(_ ([i v] ...) e1 e2 ...)
-     (all-ids? #'(i ...))
-     #'((lambda (i ...) e1 e2 ...) v ...)]
+     (if (all-ids? #'(i ...))
+         #'((lambda (i ...) e1 e2 ...) v ...)
+         (raise-syntax-error #f "non-identifier found" x))]
     [(_ name ([i v] ...) e1 e2 ...)
-     (all-ids? #'(i ...))
-     #'((letrec ((name (lambda (i ...) e1 e2 ...))) name)
-        v ...)]))
+     (identifier? #'name)
+     (if (all-ids? #'(i ...))
+         #'((letrec ([name (lambda (i ...) e1 e2 ...)]) name)
+            v ...)
+         (raise-syntax-error #f "non-identifier found" x))]))
 
-(print-test (let_4 ([v1 '1]
+(print-test (let_5 ([v1 '1]
                     [v2 '3]
                     [v3 '3]
                     [v4 '7])
                    (format "~s~s~s~s" v1 v2 v3 v4))
 
-            (let_4 fac ([n 10])
+            (let_5 fac ([n 10])
                    (if (zero? n)
                        1
                        (* n (fac (sub1 n)))))
 
-            ; "v1" as identifier will report a bad syntax error.
-            (let_4 (["v1" '1])
+            ; "v1" raises a syntax error with message "non-identifier
+            ; found".
+            (let_5 (["v1" '1])
                    (format "~s" v1)))
