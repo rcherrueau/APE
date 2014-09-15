4a7c4836c2a4b4cf2ead63d299e8b44c13d49f32
Rl3x Fish ⚓ <RonanCherrueau@gmail.com>
2014-09-15 19:36:31 +0200

    KeyMker

--- scala/typb/typbTypechecking.scala
+++ scala/typb/typbTypechecking.scala
@@ -2,25 +2,30 @@
 // http://stackoverflow.com/a/12937819
 // https://github.com/milessabin/shapeless
 
-import scala.util.Random
-
-
 // The program enventualy type checks! Next step is in store, try make
-// the key implicit.
+// the key implicit: there is no way to do that!
 
 object Cloud {
-  abstract class Key(id: Long) { type Nature }
-
   import collection.mutable.Map
+  object KeyMaker {
+    import scala.util.Random
+    object Raw {
+      def apply() = new Key(Random.nextLong()) { type Nature = Raw }
+    }
+    object Encrypted {
+      def apply() = new Key(Random.nextLong()) { type Nature = Encrypted }
+    }
+  }
+
+  abstract class Key(id: Long) { type Nature }
 
   val database = Map.empty[Key, Any]
 
   def read(key: Key): Option[key.Nature] =
     database.get(key).asInstanceOf[Option[key.Nature]]
 
-  def store(data: Nature)(key: Key): Unit = {
+  def store(data: Nature)(key: Key): Unit =
     database.update(key, data)
-  }
 }
 
 sealed abstract class Nature
@@ -29,16 +34,21 @@ case class Encrypted extends Nature
 
 object TYPB_TypeChecking {
   def main(args: Array[String]) {
-    import Cloud.Key
+    import Cloud.KeyMaker
 
-    val rawKey = new Key(Random.nextLong()) { type Nature = Raw }
+    val rawKey = KeyMaker.Raw()
     Cloud.store(Raw())(rawKey)
     val rawData: Option[Raw] = Cloud.read(rawKey)
 
-    val encKey = new Key(Random.nextLong()) { type Nature = Encrypted }
-    Cloud.store(Encrypted())(encKey)
-    val encData: Option[Encrypted] = Cloud.read(encKey)
+    val encKey1 = KeyMaker.Encrypted()
+    Cloud.store(Encrypted())(encKey1)
+    val encData1: Option[Encrypted] = Cloud.read(encKey1)
+
+    val encKey2 = KeyMaker.Encrypted()
+    Cloud.store(Encrypted())(encKey2)
+    val encData2: Option[Encrypted] = Cloud.read(encKey2)
   }
+
   // def scenario1 {
   //   val data = Alice getPrivate ;
   //   Cloud store data
