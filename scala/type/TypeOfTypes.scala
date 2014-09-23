3d66c2bf46482e6ca7c817d4818888fe8ec0bb5d
Rl3x Fish ⚓ <RonanCherrueau@gmail.com>
2014-09-23 18:32:11 +0200

    Types

--- scala/type/TypeOfTypes.scala
+++ scala/type/TypeOfTypes.scala
@@ -1,19 +1,13 @@
-/** Scala's Type of Types
-  *
-  * A full list of all trick what we can to with Types in Scala. Real
-  * life examples why we'd need these types.
-  *
-  * [[http://ktoso.github.io/scala-types-of-types/]]
-  */
-
-/** 4. Unified Type System -- Any, AnyRef, AnyVal
+/** Unified Type System -- Any, AnyRef, AnyVal
   *
   * {{{
   * scala> :load TypeOfTypes.scala
-  * scala> :javap Test4
+  * scala> :javap UnifiedTypeSystem
   * }}}
+  *
+  * [[http://ktoso.github.io/scala-types-of-types/#unified-type-system-any-anyref-anyval]]
   */
-object Test4 {
+object UnifiedTypeSystem {
   import scala.collection.mutable.ArrayBuffer
 
   class Person
@@ -40,8 +34,11 @@ object Test4 {
   // code.
 }
 
-/** 5. The Bottom Types -- Nothing and Null */
-object Test5 {
+/** The Bottom Types -- Nothing and Null
+  *
+  * [[http://ktoso.github.io/scala-types-of-types/#the-bottom-types-nothing-and-null]]
+  */
+object BottomTypes {
   // `thing' is type Int but how type inferencer can still work, and
   // infer sound types when working with "weird" situation like
   // throwing exceptions?
@@ -85,7 +82,7 @@ object Test5 {
   // TypeRef(TypeSymbol(final abstract class Null extends AnyRef))
 }
 
-/** 7. Type Variance in Scala
+/** Type Variance in Scala
   *
   * Variance can be explained as "type compatible-ness" between types:
   * {{{
@@ -98,8 +95,10 @@ object Test5 {
   *
   * In general, immutable collections are covariant and mutaboe
   * collections are invariant.
+  *
+  * [[http://ktoso.github.io/scala-types-of-types/#type-variance-in-scala]]
   */
-object Test7 {
+object TypeVariance {
   class Fruit
   case class Apple() extends Fruit
   case class Orange() extends Fruit
@@ -115,11 +114,13 @@ object Test7 {
   // val a: Array[Any] = Array[Int](1,2,3) // won't type check
 }
 
-/** 8. Refined Types (refinements)
+/** Refined Types (refinements)
   *
   * Subclassing without naming the subclass.
+  *
+  * [[http://ktoso.github.io/scala-types-of-types/#type-variance-in-scala]]
   */
-object Test8 {
+object RefinedTypes {
   class Entity {
     def persistForReal() = {}
   }
@@ -136,7 +137,8 @@ object Test8 {
   }
 }
 
-/** 9. Package Object */
+// Package Object
+// http://ktoso.github.io/scala-types-of-types/#package-object
 //
 // Package Object provide a useful pattern for "importing a bunch of
 // stuff together" as well as being one of the places to look for
@@ -168,8 +170,11 @@ object Test8 {
 // import com.garden.apples._
 // redApples foreach println
 
-/** 10. Types Alias */
-object Test10 {
+/** Type Alias
+  *
+  * [[http://ktoso.github.io/scala-types-of-types/#type-alias]]
+  */
+object TypeAlias {
   // It's a trick we can use to make our code more readable.
   type User = String
   type Age = Int
@@ -179,8 +184,11 @@ object Test10 {
   val data: Map[User, Age] = Map.empty
 }
 
-/** 11. Abstract Type Member */
-object Test11 {
+/** Abstract Type Member
+  *
+  * [[http://ktoso.github.io/scala-types-of-types/#abstract-type-member]]
+  */
+object AbstractTypeMember {
   // For Java folks, following may seem very similar to the
   // `SimplestContainer<A>' syntax. But, it's a bit more powerful.
   trait SimplestContainer {
@@ -223,19 +231,24 @@ object Test11 {
   // }
 }
 
-/** 12. F-Bounded Type
+/** F-Bounded Type
   *
   * It enables a sort of "self-referential" type constraint using to
   * solve how to define a polymorphic function that when it's called
   * at a subtype level, the polymorphic function *retains* the type of
   * the subtype.
+  *
+  * [[http://ktoso.github.io/scala-types-of-types/#self-recursive-type]]
+  * [[http://logji.blogspot.se/2012/11/f-bounded-type-polymorphism-give-up-now.html]]
   */
-object Test12 {
+object FBoundedType {
   /** F-Bounded for parameters
     *
     * Use F-Bounded Type to add constraint on a polymorphic function.
     * In this case the polymorphic function only accepts as parameter
     * subtypes of the concrete instance.
+    *
+    * [[http://ktoso.github.io/scala-types-of-types/#self-recursive-type]]
     */
   object Test1 {
     trait Fruit[T <: Fruit[T]] {
@@ -313,4 +326,196 @@ object Test12 {
     // Fortunately, calling deposit on it won't type checks:
     // Account.deposit(100, new MalignantAccount(100))
   }
+}
+
+/** 13/14. Type Constructor
+  *
+  * Act like a function but on the type level.
+  *
+  * [[http://adriaanm.github.io/research/2010/10/06/new-in-scala-2.8-type-constructor-inference/]]
+  */
+// TODO
+
+
+/** Type Class
+  *
+  * [[http://danielwestheide.com/blog/2013/02/06/the-neophytes-guide-to-scala-part-12-type-classes.html]]
+  */
+object TypeClass {
+  /** Fancy statistics library: No polymorphisme */
+  object StatisticsNoPolymorphisme {
+    def median(xs: Vector[Double]): Double = xs(xs.size / 2)
+    def quartiles(xs: Vector[Double]): (Double, Double, Double) =
+      (xs(xs.size / 4), median(xs), xs(xs.size / 4 * 3))
+    def iqr(xs: Vector[Double]): Double = quartiles(xs) match {
+      case (lowerQuartile, _, upperQuartile) => upperQuartile - lowerQuartile
+    }
+    def mean(xs: Vector[Double]): Double = xs.reduce(_ + _) / xs.size
+  }
+
+  // Now we want to support more than just double numbers. So let's
+  // implement all these methods again for Int numbbers, right? No,
+  // the type parameter suffers from type erasure!
+  //
+  //   def median(xs: Vector[Int]): Int = xs(xs.size / 2)
+  //   def quartiles(xs: Vector[Int]): (Int, Int, Int) =
+  //     (xs(xs.size / 4), median(xs), xs(xs.size / 4 * 3))
+  //
+  // > [error] method quartiles:(xs: Vector[Int])(Int, Int, Int) and
+  // > [error] method quartiles:(xs: Vector[Double])(Double, Double,
+  // > Double) at line 9
+  // > [error] have same type after erasure: (xs: collection.immutab
+  // > le.Vector)Tuple3
+
+  // As a solution, we could use subtype polymorphisme. If scala
+  // offers a subtype `Number' such that Number :> Double and Number
+  // :> Int, the library written to take a Number will work equally
+  // well when passed an Int or Double as when passed a Number.
+  //
+  // /** Fancy statistics library: Subtype polymorphisme */
+  // object StatisticsPolymorphisme {
+  //   def median(xs: Vector[Number]): Number = xs(xs.size / 2)
+  //   def quartiles(xs: Vector[Number]): (Number, Number, Number) =
+  //     (xs(xs.size / 4), median(xs), xs(xs.size / 4 * 3))
+  //   def iqr(xs: Vector[Number]): Number = quartiles(xs) match {
+  //     case (lowerQuartile, _, upperQuartile) =>
+  //       upperQuartile - lowerQuartile
+  //   }
+  //   def mean(xs: Vector[Number]): Number = {
+  //     xs.reduce(_ + _) / xs.size
+  //   }
+  // }
+  //
+  // But there is no such common trait in scala.
+  //
+  // Java developer solution is Adapter pattern.
+
+  /** Fancy statistics library: Adapter pattern */
+  object StatisticsAdapterPattern {
+    trait NumberLike[A] {
+      def get: A
+      def plus(y: NumberLike[A]): NumberLike[A]
+      def minus(y: NumberLike[A]): NumberLike[A]
+      def divide(y: Int): NumberLike[A]
+    }
+
+    case class NumberLikeDouble(x: Double) extends NumberLike[Double] {
+      def get: Double = x
+      def minus(y: NumberLike[Double]) = NumberLikeDouble(x - y.get)
+      def plus(y: NumberLike[Double]) = NumberLikeDouble(x + y.get)
+      def divide(y: Int) = NumberLikeDouble(x / y)
+    }
+
+    case class NumberLikeInt(x: Int) extends NumberLike[Int] {
+      def get: Int = x
+      def minus(y: NumberLike[Int]) = NumberLikeInt(x - y.get)
+      def plus(y: NumberLike[Int]) = NumberLikeInt(x + y.get)
+      def divide(y: Int) = NumberLikeInt(x / y)
+    }
+
+    def median[A](xs: Vector[NumberLike[A]]): NumberLike[A] = xs(xs.size / 2)
+    def quartiles[A](xs: Vector[NumberLike[A]]):
+        (NumberLike[A], NumberLike[A], NumberLike[A]) =
+      (xs(xs.size / 4), median(xs), xs(xs.size / 4 * 3))
+    def iqr[A](xs: Vector[NumberLike[A]]): NumberLike[A] =
+      quartiles(xs) match {
+        case (lowerQuartile, _, upperQuartile) =>
+          upperQuartile.minus(lowerQuartile)
+      }
+    def mean[A](xs: Vector[NumberLike[A]]): NumberLike[A] =
+      xs.reduce(_.plus(_)).divide(xs.size)
+  }
+
+  // Adapter is OK but two problems left:
+  // - User of the library has to create instances of the adapter to
+  //   interact with the library.
+  // - User of the library has to pass a `NumberLike' which is tiresome
+  //   to wrtie and read.
+
+  object Math {
+    // Interface with common operations on Number.
+    trait NumberLike[T] {
+      def plus(x: T, y: T): T
+      def divide(x: T, y: Int): T
+      def minus(x: T, y: T): T
+    }
+    object NumberLike {
+      // Rule to apply operation on Double
+      implicit object NumberLikeDouble extends NumberLike[Double] {
+        def plus(x: Double, y: Double): Double = x + y
+        def divide(x: Double, y: Int): Double = x / y
+        def minus(x: Double, y: Double): Double = x - y
+      }
+      // Rule to apply operation on Int
+      implicit object NumberLikeInt extends NumberLike[Int] {
+        def plus(x: Int, y: Int): Int = x + y
+        def divide(x: Int, y: Int): Int = x / y
+        def minus(x: Int, y: Int): Int = x - y
+      }
+    }
+  }
+
+  object StatisticsTypeClass {
+    // We put into StatisticsTypeClass's scope implicite rules of
+    // Math.NumberLike
+    import Math.NumberLike
+
+    // The implicit indicates that if no `NumberLike[T]' is passes. Then
+    // scala looks into the global scope.
+    def mean[T](xs: Vector[T])(implicit ev: NumberLike[T]): T =
+      ev.divide(xs.reduce(ev.plus(_, _)), xs.size)
+
+    def median[T](xs: Vector[T]): T = xs(xs.size / 2)
+
+    def quartiles[T](xs: Vector[T]): (T, T, T) =
+      (xs(xs.size / 4), median(xs), xs(xs.size / 4 * 3))
+
+    def iqr[T](xs: Vector[T])(implicit ev: NumberLike[T]): T =
+      quartiles(xs) match {
+        case (lowerQuartile, _, upperQuartile) =>
+          ev.minus(upperQuartile, lowerQuartile)
+      }
+
+    def main(args: Array[String]) {
+      val strs = Vector("1", "2", "3", "4")
+      println(StatisticsTypeClass.quartiles(strs))
+    }
+  }
+}
+
+/** Structural Type
+  *
+  * In general, with types we think in terms of "does it implement
+  * interface X?". With structural types we can reason about the
+  * structure of a given object. When checking whether a type matches
+  * using structual typing, we need to change our question to:"does it
+  * have a method with this signature?".
+  */
+object StructuralType {
+  // French saying: "Il ne faut pas mélanger les Torchons et les
+  // Serviettes" So in OOP there is no common supertype between
+  // `Torchon' and `Serviette'.
+  class Torchon { def wipe: Unit = {} }
+  class Serviette { def wipe: Unit = {} }
+  // But both of them have a wipe methode and we should use one or the
+  // other to clean the `Table'. Because there is no common supertype
+  // between `Torchon' and `Serviette', let's use Structural Type to
+  // define the method clean.
+  object Table {
+    def clean(wiper: { def wipe: Unit }) =
+      wiper.wipe
+  }
+  Table.clean(new Torchon())
+  Table.clean(new Serviette())
+  // Another fact to keep in mind when using Structural Typing is that
+  // it actually *has huge (negative) runtime performance*
+  // implications, as it is actually implemented using reflection.
+  /** {{{
+    * Scala> :javap StructuralType$Table$
+    * public void clean(java.lang.Object);
+    * // ...
+    * 3: invokevirtual #75                 // Method java/lang/Object.getClass:()Ljava/lang/Class;
+    * 6: invokestatic  #77                 // Method reflMethod$Method1:(Ljava/lang/Class;)Ljava/lang/reflect/Method;
+    * }}}
+    */
 }
