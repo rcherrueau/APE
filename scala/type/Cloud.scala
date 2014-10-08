1126a324d2f597cb1b527fcc6b07fc55012bd0d0
Rl3x Fish ⚓ <RonanCherrueau@gmail.com>
2014-10-08 14:17:44 +0200

    Bettr documentation in the program

--- scala/type/Cloud.scala
+++ scala/type/Cloud.scala
@@ -1,11 +1,4 @@
-import shapeless.test.illTyped
-
-// object testutil {
-//   def typeMismatch(found: String, required: String) =
-//     "type mismatch;\n  found   : " + found + "\nrequired: " + required
-// }
-
-// import testutil._
+import utils._
 
 package object privacysafer {
   // A way to encode each technques in the types system is with
@@ -34,52 +27,51 @@ object Alice extends Id
 
 object Bob extends Id
 
-/** Encryption tests from Alice and Bob */
+// Encryption tests from Alice and Bob.
 object EncryptionTests {
-  def run {
-    // Alice is the owner of the Data
-    val aliceData: Alice.IdData = Alice.Raw()
-    val aliceRawData: Alice.Raw = Alice.Raw()
-    val aliceEncData: Alice.AESECS[Alice.Raw] = Alice.encrypt(aliceRawData)
-    val aliceDecData: Alice.Raw = Alice.decrypt(aliceEncData)
+  // Alice is the owner of the Data
+  val aliceData: Alice.IdData = Alice.Raw()
+  val aliceRawData: Alice.Raw = Alice.Raw()
+  val aliceEncData: Alice.AESECS[Alice.Raw] = Alice.encrypt(aliceRawData)
+  val aliceDecData: Alice.Raw = Alice.decrypt(aliceEncData)
 
-    // Bob cannot be the owner of the Data
-    illTyped("""
-    val bobData: Bob.IdData = Alice.Raw()
-    """)
-    illTyped("""
-    val bobRawData: Bob.Raw = Alice.Raw()
-    """)
-    illTyped("""
-    val bobEncData: Bob.AESECS[Alice.Raw] = Alice.encrypt(aliceRawData)
-    """)
-    illTyped("""
-    val encBobData: Alice.AESECS[Bob.Raw] = Alice.encrypt(aliceRawData)
-    """)
-    illTyped("""
-    val bobDecData: Bob.Raw = Alice.decrypt(aliceEncData)
-    """)
+  // Bob cannot be the owner of the Data
+  illTyped("""
+  val bobData: Bob.IdData = Alice.Raw()
+  """)
+  illTyped("""
+  val bobRawData: Bob.Raw = Alice.Raw()
+  """)
+  illTyped("""
+  val bobEncData: Bob.AESECS[Alice.Raw] = Alice.encrypt(aliceRawData)
+  """)
+  illTyped("""
+  val encBobData: Alice.AESECS[Bob.Raw] = Alice.encrypt(aliceRawData)
+  """)
+  illTyped("""
+  val bobDecData: Bob.Raw = Alice.decrypt(aliceEncData)
+  """)
 
-    // `Id' cannot encrypt already encrypted data
-    illTyped("""
-    (new Id).encrypt(aliceEncData)
-    """)
+  // `Id' cannot encrypt already encrypted data
+  illTyped("""
+  (new Id).encrypt(aliceEncData)
+  """)
 
-    // `Id' cannot decrypt others Ids encrypted data
-    illTyped("""
-    (new Id).decrypt(aliceEncData)
-    """)
-  }
+  // `Id' cannot decrypt others Ids encrypted data
+  illTyped("""
+  (new Id).decrypt(aliceEncData)
+  """)
 }
 
-/** DummyCloud
-  *
-  * DummyCloud is a Cloud which offers three services:
-  * 1. Storing data in the Cloud.
-  * 2. Reading data from the Cloud.
-  * 3. Process data to generate a Chart. This service use an internal
-  *    service called `genChart'.
-  */
+// Now we construct a Cloud which offers three services: (1) storing
+// data in the Cloud, (2) Reading data from the Cloud and (3)
+// Processing data to generate a Chart. The third service uses an
+// internal service called `genChart' and `genChart' cannot process
+// encrypted data.
+//
+// In a first version, called `DummyCloud', we don't add specific
+// consrtaint on the type system. We show that this leads to an
+// undesirable program that type checks.
 object DummyCloud extends Id {
   case class Key()
   case class Chart() extends IdData
@@ -96,38 +88,31 @@ object DummyCloud extends Id {
 
   // Returns a chart from a data accessor.
   def processData(k: Key): Chart = genChart(read(k))
+
+  // Test that stores Alice's raw and encrypted data and processes it.
+  // The process type checks with raw and encrypted data which is not
+  // exactly what we want. We want that `genChart' only processes non
+  // encrypted data and thus, type checks only in th presene of non
+  // encypted data.
+  object DummyCloudTests {
+    // Storing of Raw and Encrypted data
+    val rawDataKey = DummyCloud.store(Alice.Raw())
+    val encDataKey = DummyCloud.store(Alice.encrypt(Alice.Raw()))
+
+    // Type checks with raw data -- Great!
+    DummyCloud.processData(rawDataKey)
+
+    // Type checks with encrypted data -- D'oh!
+    DummyCloud.processData(encDataKey)
+  }
 }
 
-/** DummyCloud tests
-  *
-  * The test stores Alice's raw and encrypted data and then tries to
-  * process it. The process type checks with raw and encrypted data
-  * which is not exactly what we want. We want that genChart only
-  * processes non encrypted data and thus, type checks only in th
-  * presene of non encypted data.
-  */
-object DummyCloudTests {
-  import DummyCloud.Key
-
-  // Storing of Raw and Encrypted data
-  val rawdataKey = DummyCloud.store(Alice.Raw())
-  val encdataKey = DummyCloud.store(Alice.encrypt(Alice.Raw()))
-
-  // Type checks with raw data -- Great!
-  DummyCloud.processData(rawdataKey)
-
-  // Type checks with encrypted data -- D'oh!
-  DummyCloud.processData(encdataKey)
-}
-
-/** SmartCloud
-  *
-  * SmartCloud is a Cloud which offers three services:
-  * 1. Storing data in the Cloud.
-  * 2. Reading data from the Cloud.
-  * 3. Process data to generate a Chart. This service use an internal
-  *    service called `genChart'.
-  */
+// In a second version of the Cloud, called `SmarterCloud', we add a
+// `NotEncrypted' constraint on the data of `genChart'. The constraint
+// leads to the coercion of data of `processData' to be `NotEncrypted'.
+//
+// As we show in the test part, this solution excludes program that
+// calls `processData' with encrypted data.
 object SmarterCloud extends Id {
   // T encode the type of the stored data
   case class Key[T]()
@@ -145,33 +130,49 @@ object SmarterCloud extends Id {
   private def genChart[T <: Data with NotEncrypted](d: T): Chart = ???
 
   // Doesn't type checks: the `T' should be a `NotEncrypted'
-  // def processData[T <: Data](k: Key[T]): Chart = genChart(read(k))
+  illTyped("""
+  def processData[T <: Data](k: Key[T]): Chart = genChart(read(k))
+  """)
 
   // Returns a chart from a data accessor. Here the notation `with
   // NotEncrypted' is mandatory by the type system.
   def processData[T <: Data with NotEncrypted](k: Key[T]): Chart =
     genChart(read(k))
+
+  // The test stores Alice's raw and encrypted data and then processes
+  // it. The program type checks only with raw data.
+  object SmarterCloudTests {
+    // The key identifies the form and the owner of stored data
+    val aliceRawDataKey: Key[Alice.Raw] =
+      SmarterCloud.store(Alice.Raw())
+    val aliceEncDataKey: Key[Alice.AESECS[Alice.Raw]] =
+      SmarterCloud.store(Alice.encrypt(Alice.Raw()))
+
+    // Thus, Bob couldn't claim to be the owner of a Alice stored
+    // information.
+    illTyped("""
+    val bobRawDataKey: Key[Bob.Raw] =
+    SmarterCloud.store(Alice.Raw())
+    """)
+    illTyped("""
+    val bobEncDataKey: Key[Bob.AESECS[Alice.Raw]] =
+    SmarterCloud.store(Alice.encrypt(Alice.Raw()))
+    """)
+    illTyped("""
+    val encBobDataKey: Key[Alice.AESECS[Bob.Raw]] =
+    SmarterCloud.store(Alice.encrypt(Alice.Raw()))
+    """)
+
+    // Calling `processData' with raw data type checks - Great!
+    SmarterCloud.processData(aliceRawDataKey)
+
+    // Calling `processData' with encrypted data doesn't type check -
+    // Great!
+    illTyped("""
+    SmarterCloud.processData(aliceEncDataKey)
+    """)
+  }
 }
-
-/** SmarterCloud tests
-  *
-  * The test stores Alice's raw and encrypted data and then tries to
-  * process it. The process type checks only with raw data.
-  */
-object SmarterCloudTests {
-  import SmarterCloud.Key
-
-  // Storing of Raw and Encrypted data
-  val rawdataKey = SmarterCloud.store(Alice.Raw())
-  val encdataKey = SmarterCloud.store(Alice.encrypt(Alice.Raw()))
-
-  // Type checkss with raw data - Great!
-  SmarterCloud.processData(rawdataKey)
-
-  // Doesn't type check with encrypted data - Great!
-  // SmarterCloud.processData(encdataKey)
-}
-
 
 object PullableCloud extends Id {
   // T encode the type of the stored data
@@ -188,20 +189,14 @@ object PullableCloud extends Id {
 
   // Chart generator that takes any kind of Data.
   def genChart[T <: Data with NotEncrypted](d: T): Chart = ???
-}
 
-object PullerAlice extends Id {
-  import PullableCloud._
-
-  def processData[T <: IdData](k: Key[T]): Chart =
-    PullableCloud.read(k) match {
-      case d: IdData with NotEncrypted =>
-        PullableCloud.genChart(d)
-      // case e: AESECS[IdData with NotEncrypted] =>
-      //   PullableCloud.genChart(decrypt(e))
+  object PullerAlice extends Id {
+    def processData[T <: IdData](k: Key[T]): Chart =
+      PullableCloud.read(k) match {
+        case d: IdData with NotEncrypted =>
+          PullableCloud.genChart(d)
+        // case e: AESECS[IdData with NotEncrypted] =>
+        //   PullableCloud.genChart(decrypt(e))
     }
-}
-
-object TestRunner extends App {
-  EncryptionTests.run
+  }
 }
--- scala/type/build.sbt
+++ scala/type/build.sbt
@@ -2,6 +2,16 @@ name := "scala-types"
 
 version := "1.0"
 
-scalaVersion := "2.11.2"
+// scalaVersion := "2.11.2"
 
-libraryDependencies += "com.chuusai" %% "shapeless" % "2.0.0"
+//libraryDependencies += "com.chuusai" %% "shapeless" % "2.0.0"
+
+
+scalaVersion := "2.11.2-typelevel"
+
+// libraryDependencies += "org.scala-lang" % "scala-typelevel" % scalaVersion.value
+
+resolvers += Resolver.mavenLocal
+
+unmanagedJars in Compile ++=
+  (file("utils/target/scala-2.11/") * "scala-illtyped_2.11-1.0.jar").classpath
