83dc000da2b96a94289d81c41c52bf3aaf5414ce
Rl3x Fish ⚓ <ronancherrueau@gmail.com>
2014-10-25 01:03:55 +0200

    Makes MeetingsApp polymorphic to show applied operation

--- scala/type/Meeting.scala
+++ scala/type/Meeting.scala
@@ -1,52 +1,55 @@
 import com.github.nscala_time.time.Imports._
 
 object MeetingsApp extends App {
-  case class Meeting(date: DateTime, name: String, address: String)
+  type Ord[T] = T => Ordered[T]
+
+  def meeting(date: DateTime, name: String, address: String) =
+    (date, name, address)
 
   object Calendar {
-    def meetings(db: List[Meeting],
-                 name: String,
-                 date: DateTime): List[Meeting] =
-      db.filter(m => m.name == name && m.date >= date)
+    def meetings[D: Ord, N](ts: List[(D,N,_)], name: N, date: D) =
+      for ((d,n,a) <- ts
+        if n == name;
+        if d >= date) yield (d,n,a)
   }
 
   object Stats1 {
-    def mostVisitedClient(db: List[Meeting]): String =
-      (db.foldLeft(Map.empty[String, Int])(
-        (visit: Map[String, Int], m: Meeting) => {
-          val client = m.name
+    def mostVisitedClient[N](ts: List[(_,N,_)]) =
+      (ts.foldLeft(Map.empty[N, Int])(
+        (visit: Map[N, Int], t: (_,N,_)) => {
+          val client = t._2
           visit + (client -> (visit.getOrElse(client, 1) + 1))
         })).maxBy(_._2)._1
   }
 
   object Stats2 {
-    def mostVisitedPlaces(db: List[Meeting]): Map[String, Int] =
-      db.foldLeft(Map.empty[String, Int])(
-        (visit: Map[String, Int], m: Meeting) => {
-          val place = m.address
+    def mostVisitedPlaces[A](ts: List[(_,_,A)]) =
+      ts.foldLeft(Map.empty[A, Int])(
+        (visit: Map[A, Int], t: (_,_,A)) => {
+          val place = t._3
           visit + (place -> (visit.getOrElse(place, 0) + 1))
         })
   }
 
-  val db =
-    Meeting(new DateTime(2014, 1, 1, 0, 0),  "Bob",   "a") ::
-    Meeting(new DateTime(2014, 1, 2, 0, 0),  "Chuck", "b") ::
-    Meeting(new DateTime(2014, 1, 3, 0, 0),  "Bob",   "c") ::
-    Meeting(new DateTime(2014, 1, 4, 0, 0),  "Chuck", "d") ::
-    Meeting(new DateTime(2014, 1, 5, 0, 0),  "Bob",   "e") ::
-    Meeting(new DateTime(2014, 1, 6, 0, 0),  "Bob",   "e") ::
-    Meeting(new DateTime(2014, 1, 7, 0, 0),  "Bob",   "e") ::
-    Meeting(new DateTime(2014, 1, 8, 0, 0),  "Bob",   "f") ::
-    Meeting(new DateTime(2014, 1, 9, 0, 0),  "Chuck", "b") ::
-    Meeting(new DateTime(2014, 1, 10, 0, 0), "Chuck", "g") :: Nil
+  val ts =
+    meeting(new DateTime(2014, 1, 1, 0, 0),  "Bob",   "a") ::
+    meeting(new DateTime(2014, 1, 2, 0, 0),  "Chuck", "b") ::
+    meeting(new DateTime(2014, 1, 3, 0, 0),  "Bob",   "c") ::
+    meeting(new DateTime(2014, 1, 4, 0, 0),  "Chuck", "d") ::
+    meeting(new DateTime(2014, 1, 5, 0, 0),  "Bob",   "e") ::
+    meeting(new DateTime(2014, 1, 6, 0, 0),  "Bob",   "e") ::
+    meeting(new DateTime(2014, 1, 7, 0, 0),  "Bob",   "e") ::
+    meeting(new DateTime(2014, 1, 8, 0, 0),  "Bob",   "f") ::
+    meeting(new DateTime(2014, 1, 9, 0, 0),  "Chuck", "b") ::
+    meeting(new DateTime(2014, 1, 10, 0, 0), "Chuck", "g") :: Nil
 
-  def printCalendar(db: List[Meeting]) =
-    println(db.toString().replaceAll(", ", "\n     "))
+  def printCalendar(ts: List[_]) =
+    println(ts.toString().replaceAll(", ", "\n     "))
 
-  val res1 = Calendar.meetings(db, "Bob", new DateTime(2014, 1, 6, 0, 0))
+  val res1 = Calendar.meetings(ts, "Bob", new DateTime(2014, 1, 6, 0, 0))
   printCalendar(res1)
   val res2 = Stats2.mostVisitedPlaces(res1)
   println(res2)
-  val res3 = Stats1.mostVisitedClient(db)
+  val res3 = Stats1.mostVisitedClient(ts)
   println(res3)
 }
