6e989631b522783fbf70abc435347563e65d2d43
Rl3x Fish ⚓ <ronancherrueau@gmail.com>
2014-11-26 02:20:03 +0100

    Arrow implementation

--- scala/functional-scala/LCH11a.scala
+++ scala/functional-scala/LCH11a.scala
@@ -1,4 +1,3 @@
-//
 // @Article{LCH11a,
 //   author =       {Hai Liu and Eric Cheng and Paul Hudak},
 //   title =        {Causal commutative arrows},
@@ -15,31 +14,117 @@
 
 // `=>:' stands for Function1
 trait Arrow[=>:[_,_]] {
-  /** Lifts an ordinary function */
-  def arr[A,B](f: (A) => B): A =>: B
+  /** Lifts an ordinary function.
+    *
+    * {{{
+    *    b +--arr--+ c
+    *   ---|-> f --|--->
+    *      +-------+
+    * }}}
+    *
+    */
+  def arr[B,C](f: (B) => C): B =>: C
 
   /** Composes two arrow computations by connecting the output of the
-    * first to the input of the second. f(g())
+    * first to the input of the second.
+    *
+    * {{{
+    *      +-----------compose-------------+
+    *    b |  b +--arr--+  c  +--arr--+ d  | d
+    *   ---|----|-> f --|-----|-> g --|----|--->
+    *      |    +-------+     +-------+    |
+    *      +-------------------------------+
+    * }}}
+    *
     */
-  def compose[A,B,C](f: B =>: C, g: A =>: B): A =>: C
+  def compose[B,C,D](f: B =>: C, g: C =>: D): B =>: D
 
   /** Converts an arrow computation taking one input and one result,
     * into an arrow computation taking two inputs and two results. The
     * original arrow is applied to the first part of the input, and
     * the result becomes the first part of the output. The second part
     * of the input is fed directly to the second part of the output.
+    *
+    * {{{
+    *      +------first------+
+    *    b |  b +--arr--+ c  | c
+    *   ---|----|-> f --|----|--->
+    *    d |    +-------+    | d
+    *   ---|-----------------|--->
+    *      +-----------------+
+    * }}}
+    *
     */
-  def first[A,B,C](f: A =>: B): (A,C) =>: (B,C)
+  def first[B,C,D](f: B =>: C): (B,D) =>: (C,D)
 
-  /** Dual of first */
-  def second[A,B,C](f: A =>: B): (C,A) =>: (C,B) = {
+  /** Dual of first.
+    *
+    * {{{
+    *    c +-----second------+ c
+    *   ---|-----------------|--->
+    *    d |  d +--arr--+ e  | e
+    *   ---|----|-> f --|----|--->
+    *      |    +-------+    |
+    *      +-----------------+
+    * equiv:
+    *      +---------------------second------------------------------+
+    *      |                   +------first------+                   |
+    *    c | c +---arr--+ d  d |  d +--arr--+ e  | e  e +---arr--+ c | c
+    * -----|---|->    --|------|----|-> f --|----|------|->    --|---|--->
+    *    d | d |  swap  | c  c |  c +-------+ c  | c  c |  swap  | e | e
+    * -----|---|->    --|------|-----------------|------|->    --|---|--->
+    *      |   +--------+      +-----------------+      +--------+   |
+    *      +---------------------------------------------------------+
+    * }}}
+    *
+    */
+  def second[C,D,E](f: D =>: E): (C,D) =>: (C,E) = {
     // swap in a polymorphic manner
     def swap[X,Y](t: (X,Y)): (Y,X) = t.swap
 
-    compose(arr(swap[B,C] _), compose(first[A,B,C](f), arr(swap[C,A] _)))
+    compose(arr(swap[C,D] _), compose(first[D,E,C](f), arr(swap[E,C] _)))
   }
 
-  /** Parrallel composition is the sequence of first and second */
-  def seq[A,B,C,D](f: A =>: B, g: C =>: D): (A,C) =>: (B,D) =
-    compose(first[A,B,C](f), second[C,D,B](g))
+  /** Parrallel composition is the sequence of first and second.
+    *
+    * {{{
+    *      +------------split-------------+
+    *   b1 | b1 +--arr--+ b2              | b2
+    *  ----|----|-> f --|-----------------|---->
+    *   c1 |    +-------+ c1 +--arr--+ c2 | c2
+    *  ----|-----------------|-> g --|----|---->
+    *      |                 +-------+    |
+    *      +------------------------------+
+    * }}}
+    *
+    */
+  def split[B1,B2,C1,C2](f: B1 =>: B2, g: C1 =>: C2): (B1,C1) =>: (B2,C2) =
+    compose(first[B1,B2,C1](f), second[B2,C1,C2](g))
+}
+
+object Arrow {
+  def apply[F[_,_]: Arrow]: Arrow[F] = implicitly[Arrow[F]]
+
+  implicit val f1arrow = new Arrow[Function1] {
+    def arr[B, C](f: B => C): B => C = f
+    def compose[B, C, D](f: B => C,g: C => D): B => D =
+      f andThen g
+    def first[B, C, D](f: B => C): ((B, D)) => (C, D) = {
+      case (b,d) => (f(b),d)
+    }
+  }
+
+  object TestArrow extends App {
+    // implicit def f1ToArrow(f: Function1[_, _]): Arrow[Function1] =
+    //   implicitly[Arrow[Function1]].arr(f)
+
+    val plus1 = (_: Int) + 1
+    val times2 = (_: Int) * 2
+    val rev = (_: String) reverse
+
+    implicitly[Arrow[Function1]].first(implicitly[Arrow[Function1]].arr(plus1)) apply (7, "abc")
+
+    // plus1.first apply (7, "abc")
+  }
+
 }
