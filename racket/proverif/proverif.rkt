0f5618e0769a618086bfa67edc498fc388f33134
RFish ⚓ <RonanCherrueau@gmail.com>
2015-05-27 15:02:09 +0200

    proverif system call

--- racket/proverif/proverif.rkt
+++ racket/proverif/proverif.rkt
@@ -6,12 +6,46 @@
          racket/cmdline)
 
 (cond
+  ;; Proverif is installed
   [(system "hash proverif 2>/dev/null")
+   ;; Command line
+   (define color-mode (make-parameter #t))
+   (define debug-mode (make-parameter #f))
    (define file-to-compile
      (command-line
+      #:usage-help "Cryptographic protocol verifier"
+      #:once-each
+      [("-t" "--test")  "display a bit more information for debugging"
+                        (debug-mode #t)]
+      [("--no-color")   "do not use ANSI color codes"
+                        (color-mode #f)]
       #:args (filename)
       filename))
-   (define output (open-output-file "compiled-rastache.pv" #:exists 'replace))
-   (rast-compile/render (open-input-file file-to-compile) #hash{} output)
-   (system "proverif compiled-rastache.pv")]
-  [else (displayln "I require proverif but it's not installed.  Aborting.")])
+
+   (cond
+     ;; The given file exists
+     [(file-exists? file-to-compile)
+      ;; Compile option
+      (define options
+        (let ([opts ""])
+          (when (color-mode) (set! opts (string-append "-color " opts)))
+          (when (debug-mode) (set! opts (string-append "-test " opts)))
+          opts))
+
+      ;; Expand rastache file
+      (define output-name "compiled-rastache.pv")
+      (define output (open-output-file output-name
+                                       #:exists 'replace))
+      (define tokens (rast-compile/open-file file-to-compile))
+      (rast-render tokens #hash{} output)
+
+      ;; Call proverif on expanded file
+      (displayln (format "proverif ~a ~s" options output-name))
+      (system (format "proverif ~a ~s" options output-name))]
+
+     ;; The given file doesn't exist
+     [else
+      (error (format "File error: ~s: No such file or directory" file-to-compile))])]
+
+  ;; Proverif is not installed
+  [else (error "I require proverif but it's not installed.  Aborting.")])
