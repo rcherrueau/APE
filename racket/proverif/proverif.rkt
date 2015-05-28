c7b8203a0603e3f3ca312e242f330029620b3fab
RFish ⚓ <RonanCherrueau@gmail.com>
2015-05-28 11:58:43 +0200

    Specify a context in the command line

--- racket/proverif/proverif.rkt
+++ racket/proverif/proverif.rkt
@@ -1,24 +1,33 @@
 #! /usr/bin/env racket
 #lang racket/base
 
-(require "fill-rastache/rastache/main.rkt"
+(require (file "/Users/rcherr12/prog/APE/racket/proverif/fill-rastache/rastache/main.rkt")
          racket/system
          racket/cmdline)
 
 (cond
   ;; Proverif is installed
   [(system "hash proverif 2>/dev/null")
+   ;; proverif evaluation namespace
+   (define proverif-ns (make-base-namespace))
+
    ;; Command line
    (define color-mode (make-parameter #t))
    (define debug-mode (make-parameter #f))
+   (define with-context (make-parameter #hash()))
    (define file-to-compile
      (command-line
       #:usage-help "Cryptographic protocol verifier"
       #:once-each
-      [("-t" "--test")  "display a bit more information for debugging"
-                        (debug-mode #t)]
-      [("--no-color")   "do not use ANSI color codes"
-                        (color-mode #f)]
+      [("-t" "--test")    "display a bit more information for debugging"
+                          (debug-mode #t)]
+      [("--no-color")     "do not use ANSI color codes"
+                          (color-mode #f)]
+      [("-c" "--context") context
+                          "set the context for mustache expansion"
+                          (with-context
+                            (eval (read (open-input-string context))
+                                  proverif-ns))]
       #:args (filename)
       filename))
 
@@ -37,15 +46,16 @@
       (define output (open-output-file output-name
                                        #:exists 'replace))
       (define tokens (rast-compile/open-file file-to-compile))
-      (rast-render tokens #hash{} output)
+      (rast-render tokens (with-context) output)
 
       ;; Call proverif on expanded file
       (displayln (format "proverif ~a ~s" options output-name))
-      (system (format "proverif ~a ~s" options output-name))]
+      (void (system (format "proverif ~a ~s" options output-name)))]
 
      ;; The given file doesn't exist
      [else
-      (error (format "File error: ~s: No such file or directory" file-to-compile))])]
+      (error (format "File error: ~s: No such file or directory"
+                     file-to-compile))])]
 
   ;; Proverif is not installed
   [else (error "I require proverif but it's not installed.  Aborting.")])
