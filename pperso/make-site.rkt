ccd98dec4a0549e86551bd824c305cb9f8f4720e
RFish ⚓ <RonanCherrueau@gmail.com>
2015-12-26 05:56:01 +0100

    coutinuous generation for the page perso

--- pperso/make-site.rkt
+++ pperso/make-site.rkt
@@ -2,8 +2,13 @@
 ;; Generator for my personal homepage
 #lang racket/base
 
-(require rastache ;; https://github.com/rcherrueau/rastache
-         racket/file)
+(require rastache    ;; https://github.com/rcherrueau/rastache
+         racket/file
+         racket/port
+         racket/cmdline)
+
+;; The mustache page template.
+(define template "mustache.html")
 
 ;; A web page (and its constructor).
 ;;
@@ -13,31 +18,32 @@
 ;; content: filepath of the page content
 ;; title: print `name` in the title
 (struct page (name url cmd content title?) #:transparent)
-(define (mkpage name url cmd content [title? #t])
+(define (p name url cmd content [title? #t])
   (page name url cmd content title?))
 
 ;; List of all pages.
 (define pages
-  (list (mkpage "Ronan-Alexandre Cherrueau"
-                "index.html"
-                "about"
-                "rsc/index.html"
-                #f)
-        (mkpage "Projects"
-                "projects.html"
-                "projects"
-                "rsc/proj.html")
-        (mkpage "Publications"
-                "publications.html"
-                "publications"
-                "rsc/publi.html")
-        (mkpage "Teaching"
-                "teaching.html"
-                "teaching"
-                "rsc/teaching.html")))
+  (list (p "Ronan-Alexandre Cherrueau"
+           "index.html"
+           "about"
+           "rsc/index.html"
+           #f)
+        (p "Projects"
+           "projects.html"
+           "projects"
+           "rsc/proj.html")
+        (p "Publications"
+           "publications.html"
+           "publications"
+           "rsc/publi.html")
+        (p "Teaching"
+           "teaching.html"
+           "teaching"
+           "rsc/teaching.html")))
 
 ;; The web page Mustache template compiled.
-(define tokens (rast-compile/open-file "mustache.html"))
+(define tokens (make-parameter (rast-compile/open-file template)))
+
 
 ;; Constructs the mustache context for a specific page.
 ;; page -> ctx
@@ -55,11 +61,61 @@
           (cmd      . ,(page-cmd page))
           (content  . ,(file->string (page-content page))) })
 
-;; Main
+;; Generates a specific page.
+(define (make-page page)
+  (call-with-output-file (page-url page) #:exists 'replace
+    (λ (ostream)
+      (rast-render (tokens) (make-ctx page) ostream))))
+
+;; Generates all pages of the web site.
+(define (make-pages)
+  (for-each make-page pages))
+
+;; Deamon to continuously update the generation.
+(define (daemon)
+  (define main-cust (make-custodian))
+
+  ;; When template changes: recompiles tokens and re-generates all
+  ;; pages
+  (define (handle-tplt)
+    (parameterize ([tokens (rast-compile/open-file template)])
+      (make-pages)
+      (displayln "Regenerated")))
+
+  ;; When content changes: regenerate the page.
+  (define (handle-page page)
+    (make-page page)
+    (displayln (format "~s Regenerated" (page-url page))))
+
+  ;; Observe files
+  (parameterize ([current-custodian main-cust])
+    (thread (λ ()
+              (let loop ()
+                (sync (filesystem-change-evt template))
+                (handle-tplt)
+                (loop))))
+
+    (for-each (λ (p)
+                (thread (λ ()
+                          (let loop ()
+                            (sync (filesystem-change-evt (page-content p)))
+                            (handle-page p)
+                            (loop)))))
+              pages))
+
+  ;; Shutdown on Ctrl-D
+  (displayln "Press Ctrl-D to quit")
+  (sync (eof-evt (current-input-port)))
+  (custodian-shutdown-all main-cust)
+  (displayln "Shutdown"))
+
+;; Main program
 (module* main #f
-  ;; Generates all pages of the web site.
-  (for-each (λ (page)
-              (call-with-output-file (page-url page) #:exists 'replace
-                (λ (ostream)
-                  (rast-render tokens (make-ctx page) ostream))))
-            pages))
+  (define daemon? (make-parameter #f))
+  (command-line
+   #:usage-help "Generator for my personal webpage"
+   #:once-any [("-d" "--daemon") "continuously update" (daemon? #t)])
+
+  (cond
+    [(daemon?) (daemon)]
+    [else (make-pages)]))
