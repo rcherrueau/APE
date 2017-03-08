92a7f1d7eb9df0bd6c805b210bf100ea8539e4a3
RFish ⚓ <RonanCherrueau@gmail.com>
2017-03-08 19:04:22 +0100

    Finally well parse all tested trace

--- haskell/osp-diff/src/Data/OSPDiff.hs
+++ haskell/osp-diff/src/Data/OSPDiff.hs
@@ -4,30 +4,24 @@ module Data.OSPDiff where
 
 import Prelude
 
-import Control.Applicative (empty)
+import Control.Applicative (empty, (<|>))
 import Control.Monad
 import Data.Aeson
 import Data.Aeson.Types (Parser)
 import Data.Text (Text)
-import Data.HashMap.Lazy as H (HashMap, lookup)
+import qualified Data.HashMap.Lazy as H (lookup)
 
+
+-- Utils
 (.:*) :: (FromJSON a) => Value -> [Text] -> Parser a
 (.:*) v = parseJSON <=< foldM ((maybe empty return .) . lookupE) v
   where
     lookupE :: Value -> Text -> Maybe Value
-    lookupE (Object v) key = H.lookup key v
-    lookupE _          _   = Nothing
+    lookupE (Object v') key = H.lookup key v'
+    lookupE _           _   = Nothing
 
+
 data HTTP = Post | Get | Update | Delete deriving (Show, Eq)
-
-instance FromJSON HTTP where
-  parseJSON (String v) = case v of
-    "POST"   -> pure Post
-    "GET"    -> pure Get
-    "UPDATE" -> pure Update
-    "DELETE" -> pure Delete
-    _        -> empty
-  parseJSON _          = empty
 
 data HTTPReq = HTTPReq
   { path   :: String
@@ -35,24 +29,100 @@ data HTTPReq = HTTPReq
   , query  :: String
   } deriving (Show, Eq)
 
-instance FromJSON HTTPReq where
-  parseJSON (Object v) = HTTPReq <$>
-        v .: "path"
-    <*> v .: "method"
-    <*> v .: "query"
-  parseJSON _          = empty
-
 data DBReq = DBReq
   { stmt   :: String
-  , params :: HashMap String Value
+  , params :: Value
   } deriving (Show, Eq)
 
-instance FromJSON DBReq where
-  parseJSON (Object v) = DBReq <$>
-        v .: "statement"
-    <*> v .: "params"
+data PythonReq = PythonReq
+  { name   :: String
+  , args   :: String
+  , kwargs :: String
+  } deriving (Show, Eq)
+
+data (Show a, Eq a) => TraceInfo a = TraceInfo
+  { project  :: String
+  , service  :: String
+  , req      :: a
+  } deriving (Show, Eq)
+
+data Trace = Wsgi (TraceInfo HTTPReq) [Trace]
+           | DB (TraceInfo DBReq) [Trace]
+           | RPC (TraceInfo PythonReq) [Trace]
+           | ComputeApi (TraceInfo PythonReq) [Trace]
+           | NovaImage (TraceInfo PythonReq) [Trace]
+           | NovaVirt (TraceInfo PythonReq) [Trace]
+           | NeutronApi (TraceInfo PythonReq) [Trace]
+             deriving (Show, Eq)
+
+
+instance FromJSON HTTP where
+  parseJSON (String s) = case s of
+    "POST"   -> pure Post
+    "GET"    -> pure Get
+    "UPDATE" -> pure Update
+    "DELETE" -> pure Delete
+    _        -> empty
   parseJSON _          = empty
 
+instance FromJSON HTTPReq where
+  parseJSON (Object o) = HTTPReq <$>
+        o .: "path"
+    <*> o .: "method"
+    <*> o .: "query"
+  parseJSON _          = empty
+
+instance FromJSON DBReq where
+  parseJSON (Object o) = DBReq <$>
+        o .: "statement"
+    <*> o .: "params"
+  parseJSON _          = empty
+
+instance FromJSON PythonReq where
+  parseJSON (Object o) = PythonReq <$>
+        o .: "name"
+    <*> o .: "args"
+    <*> o .: "kwargs"
+  parseJSON _          = empty
+
+instance (ReqPath a, FromJSON a) => FromJSON (TraceInfo a) where
+  parseJSON v@(Object o) = TraceInfo <$>
+        o .: "project"
+    <*> o .: "service"
+    <*> reqPath v
+  parseJSON _            = empty
+
+instance FromJSON Trace where
+  parseJSON (Object o) = do
+    c  <- o  .: "children" -- :: Parser [Trace]
+    o' <- o  .: "info"     -- :: Parser Object
+    n  <- o' .: "name"     :: Parser String
+    case n of
+      "wsgi" -> do
+        i <- parseJSON (Object o')
+        pure $ Wsgi i c
+      "db"   -> do
+        i <- parseJSON (Object o')
+        pure $ DB i c
+      "rpc"  -> do
+        i <- parseJSON (Object o')
+        pure $ RPC i c
+      "compute_api"  -> do
+        i <- parseJSON (Object o')
+        pure $ ComputeApi i c
+      "nova_image"  -> do
+        i <- parseJSON (Object o')
+        pure $ NovaImage i c
+      "vif_driver"  -> do
+        i <- parseJSON (Object o')
+        pure $ NovaVirt i c
+      "neutron_api"  -> do
+        i <- parseJSON (Object o')
+        pure $ NeutronApi i c
+      _      -> empty
+  parseJSON _          = empty
+
+
 class (FromJSON a, Show a, Eq a) => ReqPath a where
   reqPath :: Value -> Parser a
 
@@ -62,34 +132,11 @@ instance ReqPath HTTPReq where
 instance ReqPath DBReq where
   reqPath = flip (.:*) [ "meta.raw_payload.db-start", "info", "db" ]
 
-data (Show a, Eq a) => TraceInfo a = TraceInfo
-  { project  :: String
-  , service  :: String
-  , req      :: a
-  } deriving (Show, Eq)
-
-instance (ReqPath a, FromJSON a) => FromJSON (TraceInfo a) where
-  parseJSON o@(Object v) = TraceInfo <$>
-        v .: "project"
-    <*> v .: "service"
-    <*> reqPath o
-  parseJSON _            = empty
-
-data Trace = Wsgi (TraceInfo HTTPReq) [Trace]
-           | DB (TraceInfo DBReq) [Trace]
-           deriving (Show, Eq)
-
-instance FromJSON Trace where
-  parseJSON (Object v) = do
-    c  <- parseJSON =<< v  .: "children" -- :: Parser [Trace]
-    v' <- parseJSON =<< v  .: "info"     -- :: Parser Object
-    n  <- parseJSON =<< v' .: "name"     :: Parser String
-    case n of
-      "wsgi" -> do
-        i <- parseJSON (Object v')
-        pure $ Wsgi i c
-      "db"   -> do
-        i <- parseJSON (Object v')
-        pure $ DB i c
-      _      -> empty
-  parseJSON _          = empty
+instance ReqPath PythonReq where
+  reqPath v =
+    let rpc        = v .:* [ "meta.raw_payload.rpc-start", "info", "function" ]
+        computeApi = v .:* [ "meta.raw_payload.compute_api-start", "info", "function" ]
+        novaImage  = v .:* [ "meta.raw_payload.nova_image-start", "info", "function" ]
+        novaVirt   = v .:* [ "meta.raw_payload.vif_driver-start", "info", "function" ]
+        neutronApi = v .:* [ "meta.raw_payload.neutron_api-start", "info", "function" ]
+    in rpc <|> computeApi <|> novaImage <|> novaVirt <|> neutronApi
--- haskell/osp-diff/test/Test.hs
+++ haskell/osp-diff/test/Test.hs
@@ -12,9 +12,8 @@ import Test.HUnit
 import System.Exit (exitSuccess, exitFailure)
 
 import Data.OSPDiff
-import Data.Aeson (decode, FromJSON, Value(..))
+import Data.Aeson (decode, eitherDecode, FromJSON, Value(..))
 import Data.Text (pack)
-import Data.Maybe
 
 
 -- Utils
@@ -28,9 +27,13 @@ assertOSP json res = assertEqual json (Just res) (decode (packJSON json))
     packJSON s = BS.pack $ map BI.c2w s
 
 assertParsable :: String -> Assertion
-assertParsable file = do json <- BS.readFile file
-                         let res = decode json :: Maybe [Trace]
-                         assertBool ("For " ++ file) (isJust res)
+assertParsable file = do
+  json <- BS.readFile file
+  let res  = eitherDecode json :: Either String [Trace]
+  let msg  = "For " ++ file ++ either (" -- error: " ++)
+                                      (const " -- OK") res
+  let test = either (const False) (const True) res
+  assertBool msg test
 
 
 -- HTTP Code Parsing Tests
@@ -45,13 +48,11 @@ testsHTTP = TestLabel "HTTP Code Parsing" $
 
 
 -- HTTP Request Parsing Tests
-httpreq :: String
 httpreq = unwords [ "{\"path\": \"/v3\","
                   , " \"scheme\": \"http\","
                   , " \"method\": \"GET\","
                   , " \"query\": \"\"}" ]
 
-httpreqquery :: String
 httpreqquery = unwords [ "{\"path\": \"/v2/images\","
                        , " \"scheme\": \"http\","
                        , " \"method\": \"POST\", "
@@ -67,10 +68,8 @@ testsHTTPReq = TestLabel "HTTP Request Parsing" $ TestList
 
 
 -- DB Request Parsing Tests
-dbreq :: String
 dbreq = "{\"params\": {}, \"statement\": \"SELECT 1\"}"
 
-dbreqparams :: String
 dbreqparams = unwords [ "{\"params\": "
                       ,    "{\"project_id_1\": \"b59f058989c24cd28aad3fc1357df339\","
                       ,    " \"user_id_1\": \"b8c739fdb5d04d35ae9055393077553f\","
@@ -80,13 +79,22 @@ dbreqparams = unwords [ "{\"params\": "
 
 testsDBReq :: Test
 testsDBReq = TestLabel "DB Request Parsing" $ TestList
-  [ TestCase $ assertOSP dbreq (DBReq "SELECT 1" H.empty)
+  [ TestCase $ assertOSP dbreq (DBReq "SELECT 1" (Object H.empty))
   , TestCase $ assertOSP dbreqparams
       (DBReq "SELECT 1"
-        (H.fromList [ ("project_id_1", mkStrValue "b59f058989c24cd28aad3fc1357df339")
-                    , ("user_id_1", mkStrValue "b8c739fdb5d04d35ae9055393077553f")
-                    , ("issued_before_1", mkStrValue "2017-03-03T14:14:01.000000")
-                    , ("audit_id_1", mkStrValue "yVVzGy1XRoiHIj-C7GZRBQ")]))
+        (Object $ H.fromList [ (pack "project_id_1", mkStrValue "b59f058989c24cd28aad3fc1357df339")
+                             , (pack "user_id_1", mkStrValue "b8c739fdb5d04d35ae9055393077553f")
+                             , (pack "issued_before_1", mkStrValue "2017-03-03T14:14:01.000000")
+                             , (pack "audit_id_1", mkStrValue "yVVzGy1XRoiHIj-C7GZRBQ")]))
+  ]
+
+
+-- Python Request Parsing Test
+cptPythonReq = "{\"args\": \"(<nova.compute.api.API object at 0x7feed4bca690>, <nova.context.RequestContext object at 0x7feed394cad0>, u'1fbf7f34-0aff-41b2-9611-4aaba19cca3a')\", \"name\": \"nova.compute.api.API.get\", \"kwargs\": \"{'expected_attrs': ['flavor', 'info_cache', 'metadata', 'numa_topology', 'pci_devices']}\"}"
+
+testsPythonReq :: Test
+testsPythonReq = TestLabel "Python Request Parsing" $ TestList
+  [ TestCase $ assertOSP cptPythonReq (PythonReq "nova.compute.api.API.get" "(<nova.compute.api.API object at 0x7feed4bca690>, <nova.context.RequestContext object at 0x7feed394cad0>, u'1fbf7f34-0aff-41b2-9611-4aaba19cca3a')" "{'expected_attrs': ['flavor', 'info_cache', 'metadata', 'numa_topology', 'pci_devices']}")
   ]
 
 
@@ -169,37 +177,38 @@ testsTraceInfo = TestLabel "TraceInfo Parsing" $ TestList
                          (TraceInfo "keystone" "main"
                           (HTTPReq "/v2/images" Post "limit=20"))
   , TestCase $ assertOSP tinfodbreq
-                         (TraceInfo "keystone" "main" (DBReq "SELECT 1" H.empty))
+                         (TraceInfo "keystone" "main" (DBReq "SELECT 1" (Object H.empty)))
   , TestCase $ assertOSP tinfodbreqparams
                          (TraceInfo "keystone" "main"
                            (DBReq "SELECT 1"
-        (H.fromList [ ("project_id_1", mkStrValue "b59f058989c24cd28aad3fc1357df339")
-                    , ("user_id_1", mkStrValue "b8c739fdb5d04d35ae9055393077553f")
-                    , ("issued_before_1", mkStrValue "2017-03-03T14:14:01.000000")
-                    , ("audit_id_1", mkStrValue "yVVzGy1XRoiHIj-C7GZRBQ")])))
+        (Object $ H.fromList [ (pack "project_id_1", mkStrValue "b59f058989c24cd28aad3fc1357df339")
+                             , (pack "user_id_1", mkStrValue "b8c739fdb5d04d35ae9055393077553f")
+                             , (pack "issued_before_1", mkStrValue "2017-03-03T14:14:01.000000")
+                             , (pack "audit_id_1", mkStrValue "yVVzGy1XRoiHIj-C7GZRBQ") ])))
   ]
 
 
 -- Trace Parsing Test
 testsTrace :: Test
-testsTrace = TestLabel "Trace Parsing" $ TestList
-  [ TestCase $ assertParsable "test/rsc/flavor-list-fake.json"
-  , TestCase $ assertParsable "test/rsc/flavor-list-real.json"
-  , TestCase $ assertParsable "test/rsc/hypervisor-list-fake.json"
-  , TestCase $ assertParsable "test/rsc/hypervisor-list-real.json"
-  , TestCase $ assertParsable "test/rsc/image-list-fake.json"
-  , TestCase $ assertParsable "test/rsc/image-list-real.json"
-  , TestCase $ assertParsable "test/rsc/server-create-fake.json"
-  , TestCase $ assertParsable "test/rsc/server-create-real.json"
-  , TestCase $ assertParsable "test/rsc/server-show-fake.json"
-  , TestCase $ assertParsable "test/rsc/server-show-real.json"
+testsTrace = TestLabel "Trace Parsing" $ TestList $
+  map (TestCase . assertParsable)
+  [ "test/rsc/flavor-list-fake.json"
+  , "test/rsc/flavor-list-real.json"
+  , "test/rsc/hypervisor-list-fake.json"
+  , "test/rsc/hypervisor-list-real.json"
+  , "test/rsc/image-list-fake.json"
+  , "test/rsc/image-list-real.json"
+  , "test/rsc/server-create-fake.json"
+  , "test/rsc/server-create-real.json"
+  , "test/rsc/server-show-fake.json"
+  , "test/rsc/server-show-real.json"
   ]
 
 
 -- Test Main
 testsAll :: Test
 testsAll = TestList [ testsHTTP, testsHTTPReq, testsDBReq,
-                      testsTraceInfo, testsTrace ]
+                      testsPythonReq, testsTraceInfo, testsTrace ]
 
 main :: IO ()
 main = do
