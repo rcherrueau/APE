ffce06e6bf2cfe5f4633b860a0412f75573813b9
RFish ⚓ <RonanCherrueau@gmail.com>
2017-09-13 18:15:35 +0200

    [racket] f

--- racket/beautifulracket/bf.rkt
+++ racket/beautifulracket/bf.rkt
@@ -1,11 +1,5 @@
 #lang racket/base
 
-(require racket/port
-         megaparsack megaparsack/text
-         data/applicative data/monad
-         (for-syntax racket/base)
-         )
-
 ;; From http://beautifulracket.com
 
 
@@ -15,30 +9,72 @@
 ;; bf-op   ::= '>' | '<' | '+' | '-' | '.' | ','
 ;; bf-loop ::= '[' bf-prog ']'
 
+(require megaparsack megaparsack/text
+         data/applicative data/monad)
+
+;; Util parser combinator that consumes chars that are not a tokens.
+;; bf-read/p: parser → parser
+(define (bf-read/p p)
+  (define goto-next-token/p
+    (many/p (or/p letter/p digit/p space/p)))
+
+  (do goto-next-token/p
+      [r <- p]
+      goto-next-token/p
+      (pure r)))
+
 (define bf-op/p
-  (do [x <- (char-in/p "><+-.,")]
-      (pure `(bf-op ,x))))
+  (bf-read/p (do
+       [x <- (char-in/p "><+-.,")]
+       (pure `(bf-op ,x)))))
 
 (define bf-loop/p
-  (do (char/p #\[)
-      [x <- bf-prog/p]
+  (bf-read/p (do
+      (char/p #\[)
+      [x <- (or/p (try/p bf-prog/p) (pure #f))]
       (char/p #\])
-      (pure `(bf-loop ,x))))
+      (pure `(bf-loop ,x)))))
 
 (define bf-prog/p
-  (do (many/p (or/p letter/p digit/p space/p))
+  (bf-read/p (do
       [x <- (many/p (or/p bf-op/p bf-loop/p))]
-      (pure `(bf-prog ,x))))
+      (pure `(bf-prog ,x)))))
 
-(parse-string bf-prog/p "++++-+++-++-++[>++++-+++-++-++<-]>.
-                         ++++-+++-++-++[>++++-+++-++-++<-]>.")
+
+(module+ test
+  (require rackunit
+           racket/string
+           data/either)
+
+  (define (check-bf-parsed? title prog)
+    (check-pred success? (parse-string bf-prog/p prog) title))
+
+  (check-bf-parsed? "Beautifullracket"
+    "++++-+++-++-++[>++++-+++-++-++<-]>.")
+
+  (check-bf-parsed? "Busy Beaver"
+    ">+>+>+>+>+>+>+[->[>]+[->[>]+>+>+[<]+<]+<]+++++++[>+++++++++++>+<<-]>+.----.>++.")
+
+  (check-bf-parsed? "Hello World" (string-join
+    '("[]><[][]><[][]><[][]><[][]><[][]><[][]><[][]><[][]><[][]><[]"
+      "[]>+>+>++>++<[>[->++++<<+++>]<<]>----.>->+.+++++++..+++.<+[]"
+      "[ This is hellbox, a 104 command Hello World               ]"
+      "[   >+>+>++>++<[>[->++++<<+++>]<<]>----.>>+.+++++++..+++   ]"
+      "[   .>.<<<+++++++++++++++.>>.+++.------.--------.>+.>++.   ]"
+      "[ -- Robert de Bath -- 2014                                ]"
+      "[]>>.<<<+++++++++++++++.>>.+++.------.--------.>+.+>++.<<<[]"
+      "[]><[][]><[][]><[][]><[][]><[][]><[][]><[][]><[][]><[][]><[]"
+      ) "\n")))
 
 
 ;; Reader
 ;;
+(require racket/port)
 
-;; (provide read-syntax)
+(provide read-syntax)
 
-;; (define (read-syntax src-path in)
-;;
-;;  )
+(define (read-syntax src-path in)
+  (define src-string (port->string in))
+  (define ast (parse-result! (parse-string (syntax/p bf-prog/p) src-string)))
+  `(module bf-module racket
+     ',ast))
