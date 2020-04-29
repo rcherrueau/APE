3c8936dd3956a8780c7a128e60fcfc9d7a27fc71
RFish ⚓ <RonanCherrueau@gmail.com>
2020-04-30 00:03:05 +0200

    Update car-example from CPN98

--- racket/CPN98/examples/car-example.rkt
+++ racket/CPN98/examples/car-example.rkt
@@ -1,40 +1,39 @@
-#lang s-exp "../lang.rkt"
+#lang reader "../lang.rkt"
+
+(class Unit #;... )
 
 (class Engine
-  (def (start → Unit) void)
-  (def (stop  → Unit) void))
+  (def (start → Unit) ???)
+  (def (stop  → Unit) ???))
 
-(class Driver
-  ;; ...
-  )
+(class Driver #;... )
 
-(class Driver []
-  ;; ...
-  )
-
-(class Car [n m]
-  (field [engine : (rep   / Engine)])
-  (field [driver : (world / Driver)])
+(class Car
+  (field [engine : rep/Engine])
+  (field [driver : world/Driver])
 
   (def (init → Unit)
-    (set-field! this engine (new Engine)))
+    (set-field! this engine (new rep/Engine))
+    ???)
 
-  (def (get-engine → [rep / Engine])
-    (get-field this engine))
+  (def (get-engine → rep/Engine)
+    engine)
 
-  (def (set-engine! [e : (rep / Engine)] → Unit)
+  (def (set-engine! [e : rep/Engine] → rep/Engine)
     (set-field! this engine e))
 
   (def (go → Unit)
     (send engine start)))
 
-(let ([bob : (world / Driver) (new (world / Driver))]
-      [car : (world / Car) (send (new (world / Car)) init)]
-      [_   : Unit (set-field! car driver bob)]
-      [_   : Unit (send car go)]
-      [e   : Engine (get-field car engine)]
-      [_   : Unit (send e1  stop)]
-      [e   : Engine (send car get-engine)]
-      [_   : Unit (send e1 stop)]
-      [e   : (rep / Engine) (new Engine)])
-  (send car set-engine! e))
+
+(let ([bob : world/Driver (new Driver)]
+      [car : wordl/Car    (new Car)])
+  (send car init)
+  (set-field! car driver bob)
+  (send car go)
+  (send (get-field car engine) stop) ;; fails
+  (send (send car get-engine) stop)  ;; fails but not in Java
+  (let ([e : rep/Engine (new rep/Engine)])
+    ;; fails, different rep
+    (send car set-engine! e))
+  )
